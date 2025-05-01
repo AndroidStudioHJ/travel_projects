b662d1194c168d78772915021b77376c80982039
AndroidStudioHJ <heojae424@gmail.com>
2025-05-01 10:22:57 +0900

    travel_input/views.py 적절하게 기능별로 파일 나눔

---
+++ travel_input/views/schedule.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from ..forms import ScheduleForm
+from ..models import Schedule
+
+@login_required
+def schedule_list(request):
+    schedules = Schedule.objects.filter(user=request.user)
+    return render(request, 'travel_input/schedule_list.html', {'schedules': schedules})
+
+@login_required
+def schedule_detail(request, pk):
+    schedule = get_object_or_404(Schedule, pk=pk, user=request.user)
+    return render(request, 'travel_input/schedule_detail.html', {'schedule': schedule})
+
+@login_required
+def schedule_create(request):
+    if request.method == 'POST':
+        form = ScheduleForm(request.POST)
+        if form.is_valid():
+            schedule = form.save(commit=False)
+            schedule.user = request.user
+            schedule.save()
+            messages.success(request, '일정이 성공적으로 생성되었습니다.')
+            return redirect('travel_input:schedule_detail', pk=schedule.pk)
+    else:
+        form = ScheduleForm()
+    return render(request, 'travel_input/schedule_form.html', {'form': form})
+
+@login_required
+def schedule_update(request, pk):
+    schedule = get_object_or_404(Schedule, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = ScheduleForm(request.POST, instance=schedule)
+        if form.is_valid():
+            schedule = form.save()
+            messages.success(request, '일정이 성공적으로 수정되었습니다.')
+            return redirect('travel_input:schedule_detail', pk=schedule.pk)
+    else:
+        form = ScheduleForm(instance=schedule)
+    return render(request, 'travel_input/schedule_form.html', {'form': form})
+
+@login_required
+def schedule_delete(request, pk):
+    schedule = get_object_or_404(Schedule, pk=pk, user=request.user)
+    if request.method == 'POST':
+        schedule.delete()
+        messages.success(request, '일정이 성공적으로 삭제되었습니다.')
+        return redirect('travel_input:schedule_list')
+    return render(request, 'travel_input/schedule_confirm_delete.html', {'schedule': schedule})
+
+@login_required
+def confirm_delete_all(request):
+    if request.method == 'POST':
+        Schedule.objects.filter(user=request.user).delete()
+        messages.success(request, '모든 일정이 성공적으로 삭제되었습니다.')
+        return redirect('travel_input:schedule_list')
+    return render(request, 'travel_input/confirm_delete_all.html') 
